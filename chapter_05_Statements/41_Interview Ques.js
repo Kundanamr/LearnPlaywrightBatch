88301334e6ac6bf1cc73f88a11c91718021b77c2
Kundanamr <kundanamr@gmail.com>
2026-02-27 01:43:37 +0530

    Add tasks, switch statements chapter, and rename interview question files

---
+++ chapter_05_Statements/41_Interview Ques.js
@@ -0,0 +1,25 @@
+if ([]); {
+    console.log("True")
+}
+
+// Case -1 
+let response;
+if (response) {
+
+}
+//Case - 2
+if (response !== null) {  //// Wronge
+
+}
+
+if (response != null) {   /// this is correct
+
+}
+
+///---------------------------------------------------------------------------------------------------
+if (true) {
+
+}
+else if (false) {
+
+}
