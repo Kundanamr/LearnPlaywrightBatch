24f6ae7b5d2cbf041bf6db0ece8a4ff42da7621a
Kundanamr <kundanamr@gmail.com>
2026-03-15 02:50:39 +0530

    Update Array Sorting code

--- Chapter_08-Arrays/70_Array_Sorting.js
+++ Chapter_08-Arrays/70_Array_Sorting.js
@@ -8,3 +8,14 @@ console.log(fruits);
 ///This will automaticaly sort the fruits by alphabeticaly..
 
 //Ans -  [ 'apple', 'banana', 'cherry' ]
+
+let nums = [10, 1, 21, 2];
+
+nums.sort(); // [1, 10, 2, 21] ← WRONG (compares as strings!)
+console.log(nums);
+
+nums.sort((a, b) => a - b); // Ascending
+console.log(nums);
+
+nums.sort((a, b) => b - a);
+console.log(nums); // [21, 10, 2, 1] ← descending
