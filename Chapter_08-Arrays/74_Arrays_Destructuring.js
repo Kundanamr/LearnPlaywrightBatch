d71f2c65d40bc2e4ac63cad35ee3079dc30c9ce5
Kundanamr <kundanamr@gmail.com>
2026-03-20 02:23:09 +0530

    Adding 10th March tasks and updated array destructuring

--- Chapter_08-Arrays/74_Arrays_Destructuring.js
+++ Chapter_08-Arrays/74_Arrays_Destructuring.js
@@ -8,4 +8,12 @@
 let [first, second, ...third] = [10, 20, 30, 40, 50];
 console.log(first);
 console.log(second);
-console.log(third); 
+console.log(third);
+
+
+let results = ["pass", "fail", "pass"];
+let counts = results.reduce((acc, r) => {
+    acc[r] = (acc[r] || 0) + 1;
+    return acc;
+}, {});
+console.log(counts.pass);
---
+++ Task_Practice_JS/10_ March_Task_2.js
@@ -0,0 +1,12 @@
+// let responseTimes = [320, 85, 1200, 450, 99];
+// let sorted = responseTimes.sort();
+// console.log("Fastest:", sorted[0]);
+
+
+////////Correct One
+
+let responseTimes = [320, 85, 1200, 450, 99];
+
+let sorted = responseTimes.sort((a, b) => a - b);
+
+console.log("Fastest:", sorted[0]);
